548091a1f7c7bdce268a4e36633888b23035c147
Erik <erik.frangez@gmail.com>
2016-04-27 15:04:17 +0200

    novo

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Videoteka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To je videoteka");
+            Console.WriteLine("baba");
+            Console.WriteLine("baba");
+            Console.WriteLine("baba");
+            Console.WriteLine("To je videoteka");
+            Console.WriteLine("To je videoteka");
+            Console.WriteLine("baba");
+            Console.WriteLine("baba");
+            Console.WriteLine("baba");
+            Console.WriteLine("To je videoteka");
+            Console.WriteLine("To je videoteka");
+            Console.WriteLine("To je videoteka");
+            Console.WriteLine("baba");
+            Console.WriteLine("baba");
+            Console.WriteLine("baba");
+            Console.WriteLine("To je videoteka");
+            Console.WriteLine("To je videoteka");
+            Console.WriteLine("baba");
+            Console.WriteLine("baba");
+            Console.WriteLine("baba");
+            Console.WriteLine("To je videoteka");
+            Console.WriteLine("To je videoteka");
+            Console.WriteLine("To je videoteka");
+
+            Console.WriteLine("To je videoteka");
+            Console.WriteLine("To je videoteka");
+        }
+    }
+}
